989eb39a80e001cb5cf834562eb9d403bcce0711
Carlos Vallory <carlvallory@gmail.com>
2026-01-14 15:44:10 -0300

    fix: apply lazy db loading to all endpoints

--- api/db.js
+++ api/db.js
@@ -1,7 +1,16 @@
 import { Pool } from '@neondatabase/serverless';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+// Singleton pattern for Serverless
+let pool;
 
-export default pool;
+export default function getPool() {
+    if (!pool) {
+        if (!process.env.DATABASE_URL) {
+            throw new Error("DATABASE_URL is missing");
+        }
+        pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+        });
+    }
+    return pool;
+}
